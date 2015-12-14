accbc41c95f2418ab92d8a6f1711cfdc65bb37f8
Tringer <michael.simmet@web.de>
2015-12-14 20:53:26 +0100

    Final Crawler
    Final version of crawler Project 1

--- crawler.py
+++ crawler.py
@@ -32,11 +32,21 @@ def bib_download(urls):
 
 def get_links(url):
     links = []
-    with urlopen(url) as response:
-        bs = BeautifulSoup(response, "html.parser")
-        for lnk in bs.find("body").findAll("a"):
-            if "href" in lnk.attrs:
-                links.append(lnk.attrs["href"])
+    try:
+        response = urlopen(url)
+    except ValueError as e:
+        logging.error("URL/HTTP error @ {} : {}".format(url, e))
+        return None
+    except URLError as e:
+            logging.error("URL/HTTP error @ {} : {}".format(url, e))
+            return None
+    else:
+        if response.status == 200:
+            bs = BeautifulSoup(response, "html.parser")
+            for lnk in bs.findAll("a"):
+                if "href" in lnk.attrs:
+                    links.append(lnk.attrs["href"])
+
     return links
 
 
@@ -47,46 +57,51 @@ def crawl(urls, level=1):
     for url in range(len(urls)):
         with urlopen(urls[url]) as response:
             bs = BeautifulSoup(response, "html.parser")
-            for lnk in bs.find("body").findAll("a", href=re.compile(".*bib$")):
+            for lnk in bs.findAll("a", href=re.compile(".*bib$")):
                 if "href" in lnk.attrs:
                     links.append(lnk.attrs["href"])
-        all_links[0].extend(get_links(urls[url]))
+        all_links[0] = get_links(urls[url])
 
         # belongs to the hop
         for lvl in range(1, level):
             for ins in range(len(all_links[lvl-1])):
                 all_links_tmp = []
-                all_links_tmp.extend(get_links(all_links[lvl-1][ins]))
-            all_links[lvl-1].extend(all_links_tmp)
+                all_links_tmp = get_links(all_links[lvl-1][ins])
+            all_links[lvl-1] = all_links_tmp
 
-    # hop !! problem is that sometimes only half links are stored or just trash
+    # hop !! problem is that sometimes only half links are stored or even just trash or nothing for that exception in opening
     for lvl in range(1, level):
         for ln in range(1, len(all_links[lvl])):
             with urlopen(all_links[lvl][ln]) as response:
                 bs = BeautifulSoup(response, "html.parser")
-                for lnk in bs.find("body").findAll("a", href=re.compile(".*bib$")):
+                for lnk in bs.findAll("a", href=re.compile(".*bib$")):
                     if "href" in lnk.attrs:
                         links.append(lnk.attrs["href"])
 
     return links
 
 
+def getFileLocations():
+    with open("seeds.txt") as f:
+        urls = f.readlines()
+
+    all_bib_links = crawl(urls,2)
+    file_locations = bib_download(all_bib_links);
+
+    return file_locations
 
 def main():
 
 
     logging.basicConfig(format="%(levelname)s - '%(message)s'", level=logging.INFO)
-    urls = []
-    urls.append("http://www.ims.uni-stuttgart.de/institut/mitarbeiter/seeker/")
-    urls.append("http://www.isg.uni-konstanz.de/publications/")
-    # urls.append("http://www.informatik.uni-konstanz.de/cvia/publications/")
-    # add more urls here
+    with open("seeds.txt") as f:
+        urls = f.readlines()
 
     # naked_url needed for some pages, have to be known
     # all_links = crawl(start_url, naked_url)
-    all_bib_links = crawl(urls)
+    all_bib_links = crawl(urls,2)
     file_locations = bib_download(all_bib_links);
-    print(file_locations)
+    # print(file_locations)
 
 
 if __name__ == "__main__":
